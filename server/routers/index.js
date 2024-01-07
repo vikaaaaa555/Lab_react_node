ba72e1cfdcd7d870cb24e59d6be4075630bc47e8
Victoryia Zviahintsava <vika.zv04@yandex.ru>
2024-01-07 12:39:49 +0300

    edit indexes

--- server/routers/index.js
+++ server/routers/index.js
@@ -1,10 +1,16 @@
 const Router = require('express')
 const router = new Router()
 
-router.use('/user', )
-router.use('/instructor', )
-router.use('/workout', )
-router.use('/membership', )
-router.use('/enrollment', )
+const userRouter = require('./userRouter')
+const instructorRouter = require('./instructorRouter')
+const workoutRouter = require('./workoutRouter')
+const membershipRouter = require('./membershipRouter')
+const enrollmentRouter = require('./enrollmentRouter')
+
+router.use('/user', userRouter)
+router.use('/instructor', instructorRouter)
+router.use('/workout', workoutRouter)
+router.use('/membership', membershipRouter)
+router.use('/enrollment', enrollmentRouter)
 
 module.exports = router
